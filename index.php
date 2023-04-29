f5fa81f761e86febad5b0cd121c51307ae422b9b
Rizq Saputra <rizky002211@gmail.com>
2023-04-29 19:37:20 +0800

    memperbaiki login dan register

--- index.php
+++ index.php
@@ -17,9 +17,13 @@ if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['role
             $_SESSION['nama'] = $row['nama'];
             header("location: admin/index.php");
         } else {
-            echo "<script>
-                alert('Username dan password salah.');
-            </script>";
+            echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                <strong>Gagal Login !</strong> Periksa kembali username dan password atau role anda.
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+              </div>";
+            // echo "<script>
+            //     alert('Username dan password salah.');
+            // </script>";
         }
     } else if ($role == 'staff') {
         $query = "SELECT * FROM login WHERE username='$username' AND password='$password' AND role='$role'";
@@ -31,9 +35,10 @@ if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['role
             $_SESSION['nama'] = $row['nama'];
             header("location: staff/index.php");
         } else {
-            echo "<script>
-                alert('Username dan password salah.');
-            </script>";
+            echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                <strong>Gagal Login !</strong> Periksa kembali username dan password atau role anda.
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+              </div>";
         }
     } else if ($role == 'user') {
         $query = "SELECT * FROM login WHERE username='$username' AND password='$password' AND role='$role'";
@@ -45,15 +50,16 @@ if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['role
             $_SESSION['nama'] = $row['nama'];
             header("location: user/beranda.php");
         } else {
-            echo "<script>
-                alert('Username dan password salah.');
-            </script>";
+            echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                <strong>Gagal Login !</strong> Periksa kembali username dan password atau role anda.
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+              </div>";
         }
     } else {
         echo "Role tidak valid!";
     }
 }
-mysqli_close($koneksi);
+// mysqli_close($koneksi);
 ?>
 
 
@@ -62,50 +68,54 @@ mysqli_close($koneksi);
 <head>
     <title>Login</title>
     <!-- load Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!-- load custom CSS -->
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="asset/css/login.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Masuk</h3>
-                    </div>
-                    <div class="panel-body">
-                        <form role="form" action="index.php" method="post">
-                            <fieldset>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-5">
+                <div class="card">
+                    <div class="login-panel panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Masuk</h3>
+                        </div>
+                        <div class="card-body">
+                            <form role="form" action="index.php" method="post">
                                 <div class="form-group">
-                                    <label>Username<span class="text-danger">*</span></label>
-                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
+                                    <label>Username <span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" required>
                                 </div>
+                                <br>
                                 <div class="form-group">
-                                    <label>Password<span class="text-danger">*</span></label>
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                    <label>Password <span class="text-danger">*</span></label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter password" required>
                                 </div>
-                                <div class="form-group">
-                                    <label>Role<span class="text-danger">*</span></label>
-                                    <br>
+                                <br>
+                                <div class="form-group">                    
+                                    <label>Role <span class="text-danger">*</span></label>
                                     <select class="form-control" aria-label="Default select example" name="role">
                                         <option value="admin">Admin</option>
                                         <option value="staff">Staff</option>
                                         <option value="user">User</option>
                                     </select>
                                 </div>
-                                <button type="submit" class="btn btn-lg btn-success btn-block">Masuk</button>
-                            </fieldset>
-                            <br>
-                            Belum punya akun ? <a href="register.php">Daftar</a>
-                        </form>
+                                <br>
+                                <div class="tombol">
+                                    <button type="submit" name="submit" class="btn btn-lg btn-success">Masuk</button>
+                                </div>
+                                <br>
+                                Belum punya akun ? <a href="register.php">Daftar</a>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- load Bootstrap JS -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
