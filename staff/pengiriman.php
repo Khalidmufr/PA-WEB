bb035a75f28e50a6328199c593e3b6b15c24baa1
unknown <you@example.com>
2023-05-01 12:25:36 +0800

    membuat halaman index, pengiriman, dan pembelian

--- staff/pengiriman.php
+++ staff/pengiriman.php
@@ -13,7 +13,6 @@ if ($_SESSION['role'] !== 'staff') {
     header('Location: ../index.php');
     exit();
   } ?>
-  
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -25,6 +24,8 @@ if ($_SESSION['role'] !== 'staff') {
         <link rel="icon" href="../asset/gambar/Ud Haderah.png">
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/style.css" rel="stylesheet" />
+        <link href="css/style-login.css" rel="stylesheet" />
+
     </head>
     <style>
         .navbar {
@@ -43,18 +44,17 @@ if ($_SESSION['role'] !== 'staff') {
         <div class="d-flex" id="wrapper">
             <!-- Sidebar-->
             <div id="sidebar-wrapper">
-                <div class="sidebar-heading">Staff</div>
+                <div class="sidebar-heading">Admin</div>
                 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
                     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                      <img src="../asset/gambar/Ud Haderah.png" class="bi me-2" width="250" height="50">
+                    <img src="../asset/gambar/Ud Haderah.png" class="bi me-2" width="250" height="50">
                     </a>
                     <hr>
                     <ul class="nav nav-pills flex-column mb-auto">
                       <li>
-                
-                      <a href="#" class="nav-link text-white">
-                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
-                        Beranda
+                      <a href="index.php" class="nav-link text-white">
+                          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+                          Beranda
                         </a>
                       </li>
                       <li>
@@ -63,7 +63,7 @@ if ($_SESSION['role'] !== 'staff') {
                           Pengiriman
                         </a>
                       </li>
-                      <li>                        
+                      <li>
                         <a href="pembelian.php" class="nav-link text-white">
                           <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
                           Pembelian
@@ -71,7 +71,8 @@ if ($_SESSION['role'] !== 'staff') {
                         <a href="?logout=true" class="nav-link text-white">
                           <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
                           Logout
-                        </a><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                        </a>
+                        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
                   </div>
             </div>
             <!-- Page content wrapper-->
@@ -79,20 +80,60 @@ if ($_SESSION['role'] !== 'staff') {
                 <!-- Top navigation-->
                 <nav class="navbar navbar-expand-lg navbar-light border-bottom">
                     <div class="container-fluid">
+                       
+                       
                         <svg id="sidebarToggle" xmlns="http://www.w3.org/2000/svg" width="16" height="39" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                           </svg>
                     </div>
                 </nav>
                 <!-- Page content-->
-                <div class="isi">
-                  <h1>Selamat Datang Staff</h1>                                  
+                <div class="container-fluid">
+                <div class="Isi">
+                  <h1>Form Input</h1>
+                  <form>
+                    <span class="sub">nama</span>
+                    <input type="text" id="ca" placeholder="Masukkan Nama" name="c">
+                    <span class="sub">umur</span>
+                    <input type="text" id="ba" placeholder="Masukkan umur" name="b">          
+                    <br>            
+                    <input type="submit" value="submit" cursorshover="true" onclick="tambahData()">
+                  </form>
+                    <br>
+                    
+                    <h1 class="text-center">Tabel Supir</h1>
+                    <br>
+                    <table id="tabel" class="table table-hover" border="2" cellspacing="0" width="100%">
+                        <tr>
+                          <th rowspan="1" bgcolor="yellowgreen">id_supir</th>
+                          <th rowspan="1" bgcolor="yellowgreen">Nama</th>
+                          <th rowspan="1" bgcolor="yellowgreen">Umur</th>
+                          <th colspan="1" bgcolor="yellow">Aksi</th>
+                        </tr>  
+
+                        <?php 
+            include "../koneksi.php" ;
+            $query = "SELECT * FROM supir;";
+            
+            $data = mysqli_query($koneksi,$query) ;
+            while ($row = mysqli_fetch_array($data)) {
+            ?>     
+
+                        <tr>
+                          <td><?php echo $row['id_supir'] ; ?></td>
+                          <td><?php echo $row['nama'] ; ?></td>
+                          <td><?php echo $row['umur'] ; ?></td>
+                          <td><button>aksi</button></td>
+                        </tr>
+
+                <?php } ?>
+
+                    </table>
                 </div>
-              </div>
+                </div>
             </div>
         </div>
-      </body>
-      <footer>
+        <footer>
           <div class="foot">
               <img src="../asset/gambar/Ud Haderah.png" width="150px">
               <p> Hak Cipta © 2023 - Kelompok 3 C1</p>
@@ -102,4 +143,6 @@ if ($_SESSION['role'] !== 'staff') {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
+
+    </body>
 </html>
